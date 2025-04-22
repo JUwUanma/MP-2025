2322b91a11a2a4cd3049a563a91711dee22e732f
Alejandro Núñez Fedriani <alexnufe2006@gmail.com>
2025-04-22 20:01:50 +0200

    ANF_main

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Interfaz.h"
+#include "Datos.h"
+#include "logica.h"
+#include "Tablero.h"
 
 int main() {
-    MenuPrincipal();
+    
+
+    ConfiguracionJuego config=cargar_config();
+    ConfiguracionJuego *Pconfig = &config;
+    ControlPartida ControlP = cargar_controlPartida(config);
+    Registro_Maquina reg_maq;
+    
+
+
+    MenuPrincipal(config, ControlP, reg_maq);
     return 0;
 }
-
